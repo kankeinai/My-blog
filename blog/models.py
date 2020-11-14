a633f042a72aed08e6d17a2a92b4f290a4c67d2b
begantsovaM <56974757+begantsovaM@users.noreply.github.com>
2020-11-15 05:34:17 +0600

    Some changes in structure of blog post

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 # blog/models.py
-
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
 
 class Post(models.Model):
@@ -9,7 +10,14 @@ class Post(models.Model):
         'auth.User',
         on_delete=models.CASCADE,
     )
+    details = models.CharField(max_length=200)
     body = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
 
     def __str__(self):
         return self.title
