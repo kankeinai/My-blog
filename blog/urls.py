45ae9d472375e70d0c2dac855d5d7440173d4a83
kankeinai <56974757+begantsovaM@users.noreply.github.com>
2020-11-15 17:03:17 +0600

    added feature to add/edit posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,12 @@
 # blog/urls.py
 from django.urls import path
+from . import views
 
 from .views import BlogListView, BlogDetailView
 
 urlpatterns = [
     path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
     path('', BlogListView.as_view(), name='home'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-
+from django.utils import timezone
 # Create your views here.
 from django.views.generic import ListView, DetailView
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import PostForm
 
 from .models import Post
 
@@ -14,3 +16,32 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):  # новое
     model = Post
     template_name = 'post_detail.html'
+
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'post_edit.html', {'form': form})
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'post_edit.html', {'form': form})
