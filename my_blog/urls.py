c706949bcc1fd7e25853ef4155d9aa575b9ea31d
kankeinai <56974757+begantsovaM@users.noreply.github.com>
2020-11-17 23:49:46 +0600

    additional commit

--- my_blog/urls.py
+++ my_blog/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('accounts/login/', views.LoginView.as_view(), name='login'),
     path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
+    
 ]
